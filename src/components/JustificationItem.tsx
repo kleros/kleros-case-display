0cf3e66849798889fbdf914c8c6127ea5d4a1290
alcercu <333aleix333@gmail.com>
2023-11-09 12:06:03 +0100

    fix: split justification at \n and add break words

--- src/components/JustificationItem.tsx
+++ src/components/JustificationItem.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { Locale } from "@i18n";
 import { useTranslation } from "@i18n/client";
 import { Justification } from "@utils/types";
@@ -13,6 +14,8 @@ const JustificationItem: React.FC<JustificationItemInterface> = ({
   justification,
 }) => {
   const t = useTranslation(lang, "case");
+  const justificationLines = justification.split(/\\r\\n|\\n|\\r|\n|\r|\r\n/);
+
   return (
     <div className="w-full py-4 flex">
       <span className="w-8 pr-2 flex flex-col items-end text-slate-300">
@@ -21,8 +24,13 @@ const JustificationItem: React.FC<JustificationItemInterface> = ({
           #<strong className="text-xl text-semibold">{voteID}</strong>
         </span>
       </span>
-      <div className="pl-2 w-full flex flex-col border-l-2 border-slate-300 break-words">
-        {justification}
+      <div className="pl-2 w-full flex flex-col border-l-2 border-slate-300 break-words text-justify">
+        {justificationLines.map((line, i) =>
+          <React.Fragment key={line + i}>
+            {line}
+            {i < justificationLines.length - 1 ? <br /> : null}
+          </React.Fragment>
+        )}
       </div>
     </div>
   );
