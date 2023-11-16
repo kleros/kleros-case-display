c38b170020732da571e549904bad8becb017a0e3
alcercu <333aleix333@gmail.com>
2023-11-16 16:06:30 +0100

    fix: detect urls in justification body and replace them with links

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -23,3 +23,7 @@ export function shortenAddress(address: string, chars = 4) {
     42 - chars
   )}`;
 }
+
+export const regex = {
+  url: /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g
+}
